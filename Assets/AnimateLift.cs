e80bb15759d632976b32c5692ca3faed6df13769
Darrian <rikkuness@gmail.com>
2018-10-30 11:58:37 +0000

    Update AnimateLift.cs

--- Assets/AnimateLift.cs
+++ Assets/AnimateLift.cs
@@ -18,66 +18,53 @@ public class AnimateLift : MonoBehaviour
 
   private bool goingUp = false;
 
-public Transform player;
-  void OnEnable()
-  {
+  public Transform player;
+
+  void OnEnable() {
     pos1 = new Vector3(gameObject.transform.localPosition.x, top, gameObject.transform.localPosition.z);
     pos2 = new Vector3(gameObject.transform.localPosition.x, bottom, gameObject.transform.localPosition.z);
   }
 
-  public bool IsMoving()
-  {
+  public bool IsMoving() {
       return goingDown || goingUp;
   }
 
-  public bool IsAtTop()
-  {
+  public bool IsAtTop() {
       return player.transform.position.y >= top;
     //   return gameObject.transform.localPosition.y >= top;
   }
 
-  public bool IsAtBottom()
-  {
+  public bool IsAtBottom() {
       return gameObject.transform.localPosition.y <= bottom;
   }
 
-  public void GoDown()
-  {
+  public void GoDown() {
     if (!IsMoving())
         goingDown = true;
         liftsound.Play();
   }
 
-  public void GoUp()
-  {
+  public void GoUp() {
     if (!IsMoving())
         goingUp = true;
         liftsound.Play();
   }
 
-  void Update()
-  {
-    if (IsAtTop() && goingUp)
-    {
+  void Update() {
+    if (IsAtTop() && goingUp) {
       goingUp = false;
     }
 
-    if (IsAtBottom() && goingDown)
-    {
+    if (IsAtBottom() && goingDown) {
       goingDown = false;
     }
 
-    if (goingDown)
-    {
+    if (goingDown) {
       transform.localPosition = Vector3.MoveTowards(transform.localPosition, pos2, Time.deltaTime * speed);
       player.position = Vector3.MoveTowards(player.position, new Vector3(player.position.x, bottom, player.position.z), Time.deltaTime * speed);
-
-    }
-    else if (goingUp)
-    {
+    } else if (goingUp) {
       transform.localPosition = Vector3.MoveTowards(transform.localPosition, pos1, Time.deltaTime * speed);
       player.position = Vector3.MoveTowards(player.position, new Vector3(player.position.x, top, player.position.z), Time.deltaTime * speed);
     }
-
   }
 }
